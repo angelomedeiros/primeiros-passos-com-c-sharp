afb54bad930c011f0fce31d8cbf56ac12fd5d0cf
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-27 00:39:13 -0300

    hello world

---
+++ projeto1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto1{
+    class Program{
+        static void Main(string[] args){
+            double x = 3.141592;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F5"));
+            Console.WriteLine(x.ToString("F5", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de x: " + x.ToString("F5", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
